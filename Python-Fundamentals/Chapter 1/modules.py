1ebe758b64f73f3887283cd5fdef04f4013feaff
Shahzaib Baig <78021129+Shahzaib695@users.noreply.github.com>
2026-03-31 15:21:32 +0500

    ..

---
+++ Python-Fundamentals/Chapter 1/modules.py
@@ -0,0 +1,7 @@
+import pyjokes
+joke = pyjokes.get_joke()
+print (joke)
+print("Hello World")
+
+# there are 2 types of modules built in modules and external modules
+# python supports REPEL i mean calculations ho sakti daba kr 3sre pe esa act krta hai jese calculator
---
+++ Python-Fundamentals/Chapter2/Variable.py
@@ -0,0 +1,12 @@
+print('''There are 5 different data types:
+      1 is Integer
+      2 is String
+      3 is Float
+      4 is Boolean
+      5 is None
+      ''');
+a = 10;
+b = 20;
+name =  input("Enter Your Name: ")
+print("My Name is " + name);
+print("The Name of variable can be from a-z 1-9 or _. But the name of a variable can only start from _ or a-z");
